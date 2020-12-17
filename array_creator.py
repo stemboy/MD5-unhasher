1b9faf0c55853c576323eebd34732a4d57addc1e
jj b <jj@thebabers.at>
2020-12-17 19:56:53 +0000

    basic password maker

--- array_creator.py
+++ array_creator.py
@@ -4,15 +4,38 @@ config = json.load(open("config.json", "r"))
 
 possibleLetters = "".join([config["password_character_types"][name]
                            for name in config["password_content"]["character_types"]])
-print("Possible Letters:", possibleLetters)
-print("\n")
-
-for pwd_length in range(config["password_content"]["min_length"], config["password_content"]["max_length"] + 1):
-    print("Generating password with", pwd_length, "letters")
-
-    current_psw = "".join([possibleLetters[0] for i in range(pwd_length)])
-    print("Password starting at ", current_psw)
 
 
+def loop_digit(current_str, place, pswds, outer=False):
+    for letter in possibleLetters:
+        current_str[place - 1] = letter
 
-    print()
+        if place != 1:
+            loop_digit(current_str, place - 1, pswds)
+
+        else:
+            pswds.append("".join(l for l in current_str))
+
+        if outer:
+            print("Outer letter maker at", letter, "in", possibleLetters)
+
+
+def create():
+    print("Possible Letters:", possibleLetters)
+    print("\n")
+
+    for pwd_length in range(config["password_content"]["min_length"], config["password_content"]["max_length"] + 1):
+        print("Generating passwords with", pwd_length, "letters")
+        print()
+
+        current_pwd = ["□" for i in range(pwd_length)]
+        pswds = list()
+        loop_digit(current_pwd, pwd_length, pswds, outer=True)
+
+        print()
+        print("Created", len(pswds), "passwords")
+
+        print()
+
+
+create()
