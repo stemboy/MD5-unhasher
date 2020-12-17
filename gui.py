303be8482d83994f581242095142027e6252b392
TheMaster6417 <Callumboi6969@gmail.com>
2020-12-17 21:46:20 +0000

    Created gui screen

--- gui.py
+++ gui.py
@@ -1,29 +1,30 @@
 import kivy
-kivy.require('2.0.0')
-
-run = True
-
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 
-class MainScreen(GridLayout):
+Decrypted = ("Testcles")
+Form = ("MD5")
 
+class HashDecrypter(GridLayout):
     def __init__(self, **kwargs):
-        super(MainScreen, self).__init__(**kwargs)
+        super().__init__(**kwargs)
+
         self.cols = 2
-        self.add_widget(Label(text='Enter text to decrypt:'))
-        self.ToDecrypt = TextInput(multiline=False)
-        self.add_widget(self.ToDecrypt)
 
+        self.add_widget(Label(text="Input hash:"))
 
+        self.todecrypt = TextInput(multiline=False)
+        self.add_widget(self.todecrypt)
 
+        self.add_widget(Label(text="Text found:"))
+        self.add_widget(Label(text=Decrypted))
 
-class DeCryptApp(App):
-
+class DecryptApp(App):
     def build(self):
-        return MainScreen()
+        return HashDecrypter()
 
-if run == True:
-    DeCryptApp().run()
+if __name__ == "__main__":
+    DecryptApp().run()
+
