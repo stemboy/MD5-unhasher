a1aa75a18e22204586fba319d9a56436565bab87
jj b <jj@thebabers.at>
2020-12-19 12:37:04 +0000

    garded whole project by if name == mane

--- md5-unhasher.py
+++ md5-unhasher.py
@@ -2,36 +2,37 @@ import sys
 import os
 from multiprocessing import freeze_support
 
-os.chdir(os.path.dirname(globals()["__file__"]))
-os.environ["KIVY_NO_ARGS"] = "1"
 
-args = sys.argv
+if __name__ == "__main__":
+    freeze_support()
+
+    os.chdir(os.path.dirname(globals()["__file__"]))
+    os.environ["KIVY_NO_ARGS"] = "1"
+
+    args = sys.argv
 
 
-freeze_support()
+    if args[-1] == "--help":
+        print("Usages md5-unhasher.py [Option]",
+              "                       --help               - Show this page",
+              "                       --gui-only           - Run only the gui",
+              "                       --array-create-only  - Run the array creator only", sep="\n")
 
+    elif args[-1] == "--gui-only":
+        from Gui import DecryptApp
+        app = DecryptApp()
+        app.run()
 
-if args[-1] == "--help":
-    print("Usages md5-unhasher.py [Option]",
-          "                       --help               - Show this page",
-          "                       --gui-only           - Run only the gui",
-          "                       --array-create-only  - Run the array creator only", sep="\n")
+    elif args[-1] == "--array-create-only":
+        from misc.array_creator import create
+        create()
 
-elif args[-1] == "--gui-only":
-    from Gui import DecryptApp
-    app = DecryptApp()
-    app.run()
+    else:
+        print("No arguments were given or the given arguments were not correct,",
+              "Usages md5-unhasher.py [Option]",
+              "                       --help               - Show this page",
+              "                       --gui-only           - Run only the gui",
+              "                       --array-create-only  - Run the array creator only", sep="\n")
+        print("Running the program as normal")
 
-elif args[-1] == "--array-create-only":
-    from misc.array_creator import create
-    create()
-
-else:
-    print("No arguments were given or the given arguments were not correct,",
-          "Usages md5-unhasher.py [Option]",
-          "                       --help               - Show this page",
-          "                       --gui-only           - Run only the gui",
-          "                       --array-create-only  - Run the array creator only", sep="\n")
-    print("Running the program as normal")
-
-    # Nothing here, no program in place yet
+        # Nothing here, no program in place yet
