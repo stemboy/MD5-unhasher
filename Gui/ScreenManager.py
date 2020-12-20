33cc92916f49414c639def14bdb1c1bf01d9a639
jj b <jj@thebabers.at>
2020-12-20 13:32:17 +0000

    added switch scren loging

--- Gui/ScreenManager.py
+++ Gui/ScreenManager.py
@@ -1,7 +1,15 @@
+from kivy import Logger
 from kivy.uix.screenmanager import ScreenManager
 
 
 class ScreenManager(ScreenManager):
+    last_screen = None
+
     def nav_buttons_selected_change(self, widget, state):
         if state == "down":
+            if self.last_screen != widget.text:
+                Logger.info("App: Switching to screen " + widget.text)
+
+            self.last_screen = widget.text
+            
             self.current = widget.text
