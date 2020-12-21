f8ffe2d42764281834ae09184c09b4ee311c332d
jj b <jj@thebabers.at>
2020-12-21 20:50:01 +0000

    fixed anamtion that dont work when leave screen

--- Gui/configScreen.py
+++ Gui/configScreen.py
@@ -2,5 +2,9 @@ from Gui.screen import Screen
 
 
 class ConfigScreen(Screen):
+    has_entered_already = False
+
     def on_enter(self):
-        self.ids["ConfigLayout"].on_enter()
+        if not self.has_entered_already:
+            self.ids["ConfigLayout"].on_enter()
+            self.has_entered_already = True
