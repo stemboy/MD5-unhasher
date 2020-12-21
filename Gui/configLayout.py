e97a91e1e0c5741a12bd5bc749240c68be35e0d5
jj b <jj@thebabers.at>
2020-12-21 20:30:40 +0000

    got positioning of items correct and added config background

--- Gui/configLayout.py
+++ Gui/configLayout.py
@@ -1,5 +1,25 @@
+from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
+from kivy.graphics import Rectangle, Color
 
 
 class ConfigLayout(BoxLayout):
-    pass
+    def on_kv_post(self, base_widget):
+        self.draw_bg()
+
+        Window.bind(size=self.draw_bg)
+
+    def draw_bg(self, *args):
+        n = 0
+        colors = (0.3, 0.3, 0.3), (0.5, 0.5, 0.5)
+
+        with self.canvas.before:
+            self.canvas.before.clear()
+
+            for child in self.children:
+                if child.__class__.__name__ != "Widget":
+
+                    Color(rgb=colors[n])
+                    Rectangle(pos=child.pos, size=child.size)
+
+                n = (n + 1) % 2
