b744207a1c002a45d2a60eb0c75dae4a32880c02
jj b <jj@thebabers.at>
2020-12-22 21:02:13 +0000

    rename alot of things so its now dataset

--- misc/array_creator.py
+++ misc/array_creator.py
@@ -22,7 +22,7 @@ if config.getboolean("character_types", "space"):
     possibleCharacters = possibleCharacters + str(config.get("string_character_types", "space"))
 
 
-def loop_digit(current_str, place, strings, hashes, encrypt_func, is_outer=False, is_pool_outer=False,
+def loop_digit(current_str, place, string_dataset, hash_dataset, encrypt_func, is_outer=False, is_pool_outer=False,
                parent_character=None):
     if place == config.getint("string_creation", "length_for_new_process"):
         pool = multiprocessing.Pool(processes=config.getint("string_creation", "processes"))
@@ -47,16 +47,17 @@ def loop_digit(current_str, place, strings, hashes, encrypt_func, is_outer=False
 
         if place == 0:
             string = "".join(_character for _character in current_str)
-            hashes.append(encrypt_func(string.encode()).hexdigest())
-            strings.append(string)
+            hash_dataset.append(encrypt_func(string.encode()).hexdigest())
+            string_dataset.append(string)
 
         elif place == config.getint("string_creation", "length_for_new_process"):
 
-            pool.apply_async(loop_digit, args=(current_str.copy(), place - 1, strings, hashes, encrypt_func),
+            pool.apply_async(loop_digit,
+                             args=(current_str.copy(), place - 1, string_dataset, hash_dataset, encrypt_func),
                              kwds={"is_pool_outer": True, "parent_character": character})
 
         else:
-            loop_digit(current_str, place - 1, strings, hashes, encrypt_func)
+            loop_digit(current_str, place - 1, string_dataset, hash_dataset, encrypt_func)
 
     if place == config.getint("string_creation", "length_for_new_process"):
         print()
@@ -74,8 +75,8 @@ def create():
     last_len = 0
 
     manager = multiprocessing.Manager()
-    all_strings = manager.list("")
-    all_hashes = manager.list("")
+    string_dataset = manager.list("")
+    hash_dataset = manager.list("")
 
     if config.get("encryption", "type") == "md5":
         encrypt_func = hashlib.md5
@@ -95,38 +96,38 @@ def create():
         start_time = time.time()
 
         current_string = ["□" for _ in range(string_length)]
-        loop_digit(current_string, string_length - 1, all_strings, all_hashes, encrypt_func, is_outer=True)
+        loop_digit(current_string, string_length - 1, string_dataset, hash_dataset, encrypt_func, is_outer=True)
 
         end_time = time.time()
 
-        print("Created", len(all_hashes) - last_len, "strings in", end_time - start_time, "seconds")
+        print("Created", len(hash_dataset) - last_len, "strings in", end_time - start_time, "seconds")
         print("\n")
 
-        last_len = len(all_hashes)
+        last_len = len(hash_dataset)
 
     usrDataDir = os.path.join(appdirs.user_data_dir(), "md5-unhasher")
 
     if not os.path.exists(os.path.join(usrDataDir, "encryption_datasets")):
         os.mkdir(os.path.join(usrDataDir, "encryption_datasets"))
-        print("Encryptions folder does not exist so was created")
+        print("encryption_datasets folder does not exist so was created")
 
     name = config.get("encryption", "type") + "_" + possibleCharacters + "_" + \
-           config.get("string_content", "min_length") + " to " + \
-           config.get("string_content", "max_length") + '.hashes_to_strings.json'
+           config.get("string_content", "min_length") + "_to_" + \
+           config.get("string_content", "max_length") + '.hash_to_string_dataset.json'
     path = os.path.join(usrDataDir, "encryption_datasets", name)
 
     with open(path, 'w') as outfile:
         start_time = time.time()
-        all_hashes_and_arrays = dict(zip(all_hashes, all_strings))
-        json.dump(all_hashes_and_arrays, outfile, indent=4)
+        all_hash_dataset_and_arrays = dict(zip(hash_dataset, string_dataset))
+        json.dump(all_hash_dataset_and_arrays, outfile, indent=4)
         end_time = time.time()
 
-        print("Saved", len(all_hashes_and_arrays), "strings in", end_time - start_time, "seconds")
+        print("Saved", len(all_hash_dataset_and_arrays), "strings in", end_time - start_time, "seconds")
         print("\n")
 
     total_end_time = time.time()
 
-    print("Generated and saved", len(all_hashes), "strings of", config["string_content"]["min_length"], "to",
+    print("Generated and saved", len(hash_dataset), "strings of", config["string_content"]["min_length"], "to",
           config.getint("string_content", "max_length"),
           "length with the characters '" + str(possibleCharacters) + "' in",
           total_end_time - total_start_time, "seconds")
